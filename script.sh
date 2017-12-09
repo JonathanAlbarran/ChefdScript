4e9f3889d76656d200731cffae8583daa5de510d
JonathanAlbarran <34385677+JonathanAlbarran@users.noreply.github.com>
2017-12-09 04:20:43 -0800

    Add files via upload

---
+++ script.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+find /home/ -type f -size +10M -exec ls -lh {} + | awk 'BEGIN {print "FileName, FilePath, FileSize, ModifiedDate"} {n=split($NF,a,"/");print a[n]","$9","$5","$6" "$7}'>output.csv
